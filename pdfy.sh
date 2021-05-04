04e2a35f0b89d5ae9910876fc406f57387589bc2
Isaac Feldman <isaaccfeldman@gmail.com>
2021-05-04 11:28:20 -0400

    added autoconvert styling

--- pdfy.sh
+++ pdfy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Convert my CV website page to pdf
-pandoc pages/CV.html -t gfm -o - | tail -n +3 | pandoc - -o docs/isaac_feldman_cv.pdf
+pandoc pages/CV.html -t gfm -o - | tail -n +3 | pandoc - -V linkcolor:blue -V geometry:margin=3cm -V fontsize=12pt -V mainfont="IBM Plex Serif"  -V monofont="IBM Plex Mono" --pdf-engine=xelatex -o docs/isaac_feldman_cv.pdf
