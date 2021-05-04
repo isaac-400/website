a05752ea829d03793a9a9672dc5f4d65c7839c07
Isaac Feldman <isaaccfeldman@gmail.com>
2021-05-04 12:07:46 -0400

    adjusted autogenerate aesthetics

--- pdfy.sh
+++ pdfy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Convert my CV website page to pdf
 rm -rf docs/isaac_feldman_cv.pdf
-pandoc pages/CV.html -t gfm -o - | tail -n +3 | pandoc - -V linkcolor:blue -V geometry:margin=3cm -V fontsize=12pt -V mainfont="IBM Plex Serif"  -V monofont="IBM Plex Mono" --pdf-engine=xelatex -o docs/isaac_feldman_cv.pdf
+pandoc pages/CV.html -t markdown -o - | tail -n +3 | pandoc - -V linkcolor:blue -V geometry:margin=2cm -V fontsize=12pt -V mainfont="IBM Plex Serif"  -V monofont="IBM Plex Mono" --pdf-engine=xelatex -o docs/isaac_feldman_cv.pdf
